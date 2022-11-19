3396820bc0b6eb680be38b95726762e6f91ef052
HumeraZaid <humerazshelia@gmail.com>
2022-11-19 00:05:19 -0600

    All notes

---
+++ src/Quiz7/Question2.java
@@ -0,0 +1,22 @@
+package Quiz7;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class Question2 {
+
+    public static void main(String[] args) {
+
+        TreeSet map = new TreeSet();
+        map.add("one");
+        map.add("two");
+        map.add("three");
+        map.add("four");
+        map.add("one");
+        Iterator it = map.iterator();
+        while (it.hasNext() )
+        {
+            System.out.print( it.next() + " " );
+        }
+    }
+}
